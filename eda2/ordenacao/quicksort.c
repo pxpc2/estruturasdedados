a538a2d56313a477c68beb2330d330f4d631d661
pxpc2 <pedro_daia@hotmail.com>
2022-08-02 20:13:49 -0300

    adding advanced version of quicksort

--- eda2/ordenacao/quicksort.c
+++ eda2/ordenacao/quicksort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "eda2/ordenacao/elementares.c"
 
 typedef int Item;
 #define key(A) (A)
@@ -8,7 +9,9 @@ typedef int Item;
 #define cmpexch(A, B) {if (less(B,A)) exch(A, B)}
 
 int separa(Item *v, int l, int r);
-void quickSort(Item *v, int l, int r);
+void quickSort_ingenuo(Item *v, int l, int r);
+void quickSort_mediana(Item *v, int l, int r);
+void sort(Item *v, int l, int r);
 
 // lembrar que é sempre intervalo fechado, i.e,
 // l e r fazem parte do vetor (tamanho 6 ent r=5)
@@ -30,11 +33,39 @@ int separa(Item *v, int l, int r)
     return j; // nova pos do pivot
 }
 
-void quickSort(Item *v, int l, int r)
+void quickSort_ingenuo(Item *v, int l, int r)
 {
     int j;
     if (r <= l) return;
     j = separa(v, l, r);
     quickSort(v, l, j-1);
     quickSort(v, j+1, r);
+}
+
+void quickSort_mediana(Item *v, int l, int r)
+{
+    int j;
+    if (r <= l) return;
+
+    int m = (l+r) / 2;
+
+    // essa condicao so deve ser usada se for utilizar o insertionSort após
+    // se nao for, entao a condicao p/ return é r <= l
+    if (r - l <= 32) // deixo os ultimos 32 para o insertionSort
+        return; 
+
+    cmpexch(v[m], v[r]);
+    cmpexch(v[l], v[m]);
+    cmpexch(v[r], v[m]);
+
+    j = separa(v, l+1, r-1);
+
+    quickSort(v, l, j-1);
+    quickSort(v, j+1, r);
+}
+
+void sort(Item *v, int l, int r)
+{
+    quickSort_mediana(v, l, r);
+    insertionSort(v, l, r); // já estará bem prox de ordenado
 }
