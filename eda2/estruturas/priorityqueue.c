4d2e02327fe263ce8e06bb931671e3db369ecafa
pxpc2 <pedro_daia@hotmail.com>
2022-08-10 12:38:56 -0300

    adding struct PQ so we can have multiple PQs

--- eda2/estruturas/priorityqueue.c
+++ eda2/estruturas/priorityqueue.c
@@ -4,38 +4,41 @@ typedef int Item;
 #define exch(A, B) { Item temp = A; (A) = B; (B) = temp; }
 #define cmpexch(A, B) { if (less(B,A)) exch(A, B) }
 
-static Item *pq;
-static int N;
+struct PQst
+{
+    Item *q;
+    int N;
+};
 
 /*
  *  Implementação de uma PQ_MAX
  *  isto é, uma PQ DECRESCENTE
  */
 
-void initPQ(int max_N)
+void initPQ(int max_N, struct PQst *pq)
 {
-    pq = malloc(sizeof(Item) * (max_N + 1));
-    N = 0;
+    pq->q = malloc(sizeof(Item) * (max_N + 1));
+    pq->N = 0;
 }
 
-int isEmpty()
+int isEmpty(struct PQst *pq)
 {
-    return (N == 0);
+    return (pq->N == 0);
 }
 
-void insert(Item v)
+void insert(Item v, struct PQst *pq)
 {
-    pq[++N] = v;
-    swim(pq, N);
+    pq->q[++pq->N] = v;
+    swim(pq->q, pq->N);
 }
 
-Item removeMax()
+Item removeMax(struct PQst *pq)
 {   // max item é o no índice 1, paizão
 
-    exch(pq[1], pq[N]); // joga o max pra última pos
+    exch(pq->q[1], pq->q[pq->N]); // joga o max pra última pos
 
-    int heap_length = N-1; // N-1 já contando com a remoção do max
-    sink(pq, 1, heap_length);
+    int heap_length = (pq->N - 1); // N-1 já contando com a remoção do max
+    sink(pq->q, 1, heap_length);
 
     /* 
      vale lembrar que a length é sempre intervalo fechado
@@ -44,7 +47,7 @@ Item removeMax()
      porém, o vetor *pq tem size N+1 
     */
 
-    return pq[N--]; // qm ficou em pq[N], e em seguida decrementa N
+    return pq->q[pq->N--]; // qm ficou em pq[N], e em seguida decrementa N
 }
 
 void swim(Item *pq, int k) // () fix_up
@@ -58,10 +61,10 @@ void swim(Item *pq, int k) // () fix_up
 
 void sink(Item *pq, int k, int l) // () fix_down
 {
-    while (2*k <= N)
+    while (2*k <= l)
     {
         int j = 2*k;
-        if (j < N && less(pq[j], pq[j+1]))
+        if (j < l && less(pq[j], pq[j+1]))
             j++;
         if (!less(pq[k], pq[j]))
             break;
