af6d269865e0e0c69006685fc7f88c0272949ad5
pxpc2 <pedro_daia@hotmail.com>
2022-08-11 17:51:34 -0300

    re-writing PQ code and deleting header

--- eda2/estruturas/priorityqueue.c
+++ eda2/estruturas/priorityqueue.c
@@ -1,79 +1,104 @@
+#include "stdio.h"
+#include "stdlib.h"
+
 typedef int Item;
 #define key(A) (A)
 #define less(A, B) ( key(A) < key(B) )
 #define exch(A, B) { Item temp = A; (A) = B; (B) = temp; }
 #define cmpexch(A, B) { if (less(B,A)) exch(A, B) }
 
-struct PQst
+typedef struct Heap
 {
-    Item *q;
-    int N;
-};
+    Item *pq;
+    int size;
+} Heap;
 
-/*
- *  Implementação de uma PQ_MAX
- *  isto é, uma PQ DECRESCENTE
- */
+void heap_init(Heap *heap, int max_size);
+void swim(Item *pq, int k);
+void sink(Item *pq, int k, int size);
+void heap_insert(Heap *heap, Item k);
+Item heap_poll(Heap *heap);
 
-void initPQ(int max_N, struct PQst *pq)
+void heap_init(Heap *heap, int max_size)
 {
-    pq->q = malloc(sizeof(Item) * (max_N + 1));
-    pq->N = 0;
+    heap->pq = malloc(sizeof(Item) * max_size + 1);
+    heap->size = 0;
 }
 
-int getMax(struct PQst *pq)
+void swim(Item *pq, int k)
 {
-    return pq->q[1];
-}
-
-int isEmpty(struct PQst *pq)
-{
-    return (pq->N == 0);
-}
-
-void insert(Item v, struct PQst *pq)
-{
-    pq->q[++pq->N] = v;
-    swim(pq->q, pq->N);
-}
-
-Item removeMax(struct PQst *pq)
-{   // max item é o no índice 1, paizão
-
-    exch(pq->q[1], pq->q[pq->N]); // joga o max pra última pos
-
-    int heap_length = (pq->N - 1); // N-1 já contando com a remoção do max
-    sink(pq->q, 1, heap_length);
-
-    /* 
-     vale lembrar que a length é sempre intervalo fechado
-     porém como não trabalhamos com índice 0, a heap com N elementos
-     já seria length "N" no intervalo fechado.
-     porém, o vetor *pq tem size N+1 
-    */
-
-    return pq->q[pq->N--]; // qm ficou em pq[N], e em seguida decrementa N
-}
-
-void swim(Item *pq, int k) // () fix_up
-{
+    // até chegar no topo (k=1), compara se o pai é menor que o filho
     while (k > 1 && less(pq[k/2], pq[k]))
     {
+        // se pai é menor que o filho, fazer a troca
         exch(pq[k], pq[k/2]);
-        k /= 2;
+        k /= 2; // e checa agora na nova pos
     }
 }
 
-void sink(Item *pq, int k, int l) // () fix_down
+void sink(Item *pq, int k, int size)
 {
-    while (2*k <= l)
+    // até chegar no fundo
+    while (2 * k <= size)
     {
         int j = 2*k;
-        if (j < l && less(pq[j], pq[j+1]))
+        if (j < size && less(pq[j], pq[j+1]))
             j++;
         if (!less(pq[k], pq[j]))
-            break;
+            break; // não precisa fazer a troca
         exch(pq[k], pq[j]);
         k = j;
     }
 }
+
+void heap_insert(Heap *heap, Item k)
+{
+    heap->pq[++heap->size] = k;
+    swim(heap->pq, heap->size);
+}
+
+Item heap_poll(Heap *heap)
+{
+    // joga o max para última pós
+    exch(heap->pq[1], heap->pq[heap->size]);
+    int new_length = heap->size - 1; // já contando a remoção
+    sink(heap->pq, 1, new_length);
+    return heap->pq[heap->size--];
+}
+
+int getMax(Heap *heap)
+{
+    return heap->pq[1];
+}
+
+/*int main()
+{
+    Heap *h = malloc(sizeof(Heap));
+    heap_init(h, 8);
+
+    heap_insert(h, 2);
+    heap_insert(h, 5);
+    heap_insert(h, 8);
+    heap_insert(h, 7);
+    heap_insert(h, 1);
+    heap_insert(h, 0);
+    heap_insert(h, 9);
+    heap_insert(h, 2);
+
+    Item r = heap_poll(h);
+    printf("removed: %d\n", r);
+
+    Item r2 = heap_poll(h);
+    printf("new removed: %d\n", r2);
+
+    Item r3 = heap_poll(h);
+    printf("new removed: %d\n", r3);
+    Item r4 = heap_poll(h);
+    printf("new removed: %d\n", r4);
+    Item r5 = heap_poll(h);
+    printf("new removed: %d\n", r5);
+
+    for (int i = 1; i <= h->size; i++)
+        printf("element %d = %d\n", i, h->pq[i]);
+
+}*/
